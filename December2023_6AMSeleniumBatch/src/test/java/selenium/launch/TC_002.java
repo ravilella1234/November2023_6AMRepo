6c3c8ea390485aa8702647ab7e3781faa2ac2121
ravilella1234 <masters.selenium@gmail.com>
2023-12-09 07:05:34 +0530

    WebDriver Methods

--- December2023_6AMSeleniumBatch/src/test/java/selenium/launch/TC_002.java
+++ December2023_6AMSeleniumBatch/src/test/java/selenium/launch/TC_002.java
@@ -3,11 +3,34 @@ package selenium.launch;
 public class TC_002 extends BaseTest
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		launch("chrome");
 		
 		navigateUrl("https://www.amazon.in");
+		
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().refresh();
+		
+		//driver.close();
+		driver.quit();
 	}
 
 }
