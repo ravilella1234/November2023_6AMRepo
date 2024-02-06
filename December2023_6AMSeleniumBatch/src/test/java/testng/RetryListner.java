395549a99c0cedd2168f7901347cc5e2b75505c4
ravilella1234 <masters.selenium@gmail.com>
2024-02-06 07:14:41 +0530

    TestNg Day-6

---
+++ December2023_6AMSeleniumBatch/src/test/java/testng/RetryListner.java
@@ -0,0 +1,25 @@
+package testng;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import javax.imageio.ImageTranscoder;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.annotations.ITestAnnotation;
+
+public class RetryListner implements IAnnotationTransformer
+{
+
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) 
+	{
+		IRetryAnalyzer r = annotation.getRetryAnalyzer();
+		
+		if(r == null)
+		{
+			annotation.setRetryAnalyzer(Retry.class);
+		}
+	}
+
+}
