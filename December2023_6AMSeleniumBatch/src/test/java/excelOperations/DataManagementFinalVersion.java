402db925b1c8c42773377ffc87efd2e39a226d41
ravilella1234 <masters.selenium@gmail.com>
2024-02-20 06:58:14 +0530

    Excel DataProvider Day-4

---
+++ December2023_6AMSeleniumBatch/src/test/java/excelOperations/DataManagementFinalVersion.java
@@ -0,0 +1,26 @@
+package excelOperations;
+
+import org.testng.annotations.Test;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.DataProvider;
+
+public class DataManagementFinalVersion 
+{
+  @Test(dataProvider = "getData")
+  public void f(Hashtable<String,String> h) 
+  {
+	  System.out.println(h.get("UserName"));
+	  System.out.println(h.get("UserPassword"));
+  }
+
+  @DataProvider
+  public Object[][] getData() throws Exception 
+  {
+	    ExcelAPI e = new ExcelAPI("C:\\Users\\ravi\\Desktop\\suitex.xlsx");
+		String sheetName = "data"; 
+		String testName = "LoginTest";
+		return DataUtil.getTableData(e, sheetName, testName);
+  }
+}
---
+++ December2023_6AMSeleniumBatch/src/test/java/excelOperations/DataUtil.java
@@ -0,0 +1,61 @@
+package excelOperations;
+
+import java.util.Hashtable;
+
+public class DataUtil 
+{
+	
+	public static Object[][] getTableData(ExcelAPI e,String sheetName, String testName) throws Exception
+	{
+				
+		// To find the matching Test case RowNumber
+		int testStartRowNum = 0;
+		while(!e.getCellData(sheetName, 0, testStartRowNum).equals(testName))
+		{
+			testStartRowNum++;
+		}
+		System.out.println("Test start row number is : " + testStartRowNum);
+		
+		
+		int colStartRowNum = testStartRowNum+1;
+		int dataStartRowNum = testStartRowNum+2;
+		
+		// Calculate the rows of data
+		int rows = 0;
+		while(!e.getCellData(sheetName, 0, dataStartRowNum+rows).equals(""))
+		{
+			rows++;
+		}
+		System.out.println("Total rows are : " + rows);
+		
+		// Calculate the cols of data
+		int cols  = 0;
+		while(!e.getCellData(sheetName, cols, colStartRowNum).equals(""))
+		{
+			cols++;
+		}
+		System.out.println("Total Cols  are : " + cols);
+		
+		// Read the test data
+		int dataRow = 0;
+		Hashtable<String, String> table;
+		Object[][] data = new Object[rows][1];
+		for(int rNum = dataStartRowNum;rNum<dataStartRowNum+rows;rNum++)
+		{
+			table = new Hashtable<String, String>();
+			for(int cNum=0;cNum<cols;cNum++)
+			{
+				//System.out.println(e.getCellData(sheetName, cNum, rNum));
+				//data[dataRow][cNum] = e.getCellData(sheetName, cNum, rNum);
+				String key = e.getCellData(sheetName, cNum, colStartRowNum);
+				String value = e.getCellData(sheetName, cNum, rNum);
+				table.put(key, value);
+			}
+			data[dataRow][0] = table;
+			dataRow++;
+		}
+	  
+	return data;
+	}
+
+}
