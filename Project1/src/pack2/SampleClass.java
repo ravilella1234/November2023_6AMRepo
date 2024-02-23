a219cec0035e6885dc9f6b0a37f444d74596bb77
ravilella1234 <masters.selenium@gmail.com>
2024-02-23 06:55:06 +0530

    added 2nd line in sampleclass

--- Project1/src/pack2/SampleClass.java
+++ Project1/src/pack2/SampleClass.java
@@ -5,6 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iam first");
+		System.out.println("iam second");
 	}
 
 }
