21d2fb8db56cbb40c75f7aa6625d3aad3155601b
ravilella1234 <masters.selenium@gmail.com>
2024-02-24 06:30:50 +0530

    added a,b,c variables in Sampleclas

--- Project1/src/pack2/SampleClass.java
+++ Project1/src/pack2/SampleClass.java
@@ -2,6 +2,7 @@ package pack2;
 
 public class SampleClass {
 
+	int a=10,b=20,c=30;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("iam first");
